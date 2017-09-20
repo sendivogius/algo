633d48b3aba174bcbc441b582f486122022cc7da
sendivogius <sendivogius@gmail.com>
2017-09-20 20:50:12 +0200

    Add completed previously hackerrank challenges

---
+++ hackerrank/hackerrank/caesar_cipher.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+// https://www.hackerrank.com/challenges/caesar-cipher-1
+int main(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int k;
+    cin >> k;
+    for(auto c : s){
+        if(c >= 'a' && c <= 'z')
+            cout << char(((c-'a')+k)%('z'-'a'+1)+'a');
+        else if(c >= 'A' && c <= 'Z')
+            cout << char(((c-'A')+k)%('Z'-'A'+1)+'A');
+        else
+            cout << c;
+    }
+    return 0;
+}
