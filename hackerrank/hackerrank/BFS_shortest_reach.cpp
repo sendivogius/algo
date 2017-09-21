b4668547da6ef34c164888a5d1b8d46914b4edb2
sendivogius <sendivogius@gmail.com>
2017-09-21 22:47:13 +0200

    Add breaking_the_records with tests

--- hackerrank/hackerrank/BFS_shortest_reach.cpp
+++ hackerrank/hackerrank/BFS_shortest_reach.cpp
@@ -9,28 +9,25 @@ using namespace std;
 //https://www.hackerrank.com/challenges/bfsshortreach
 
 
-
-
-
-int main(){
-	int queries;
-	cin >> queries;
-	while (queries--){
-		Graph g = fromStream(cin);
-		
-		int start;
-		cin >> start;
-
-		auto distances = NodeBFS(g, start-1);
-
-		for (size_t i = 0; i < distances.size(); i++){
-			if (start - 1 == i) continue;
-			if (distances[i] > 0)
-				cout << distances[i] * 6 << " ";
-			else
-				cout << distances[i] << " ";
-		}
-		cout << endl;
-	}
-
-}
+//int main(){
+//	int queries;
+//	cin >> queries;
+//	while (queries--){
+//		Graph g = fromStream(cin);
+//		
+//		int start;
+//		cin >> start;
+//
+//		auto distances = NodeBFS(g, start-1);
+//
+//		for (size_t i = 0; i < distances.size(); i++){
+//			if (start - 1 == i) continue;
+//			if (distances[i] > 0)
+//				cout << distances[i] * 6 << " ";
+//			else
+//				cout << distances[i] << " ";
+//		}
+//		cout << endl;
+//	}
+//
+//}
