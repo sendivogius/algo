a4344e90246eccecc545c519eeae2bbf880d1b5d
sendivogius <sendivogius@gmail.com>
2017-09-23 15:50:28 +0200

    Add between_two_sets

--- hackerrank/hackerrank/birthday_chocolate.cpp
+++ hackerrank/hackerrank/birthday_chocolate.cpp
@@ -22,15 +22,15 @@ int solveBirthday(vector<int> s, int day, int month){
 	return cnt;
 }
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> s(n);
-    for(int s_i = 0; s_i < n; s_i++){
-       cin >> s[s_i];
-    }
-    int day, month; 
-    cin >> day >> month;
-    cout << solveBirthday(s, day, month) << endl;
-    return 0;
-}
+//int main() {
+//    int n;
+//    cin >> n;
+//    vector<int> s(n);
+//    for(int s_i = 0; s_i < n; s_i++){
+//       cin >> s[s_i];
+//    }
+//    int day, month; 
+//    cin >> day >> month;
+//    cout << solveBirthday(s, day, month) << endl;
+//    return 0;
+//}
---
+++ hackerrank/hackerrank/math_utils.h
@@ -0,0 +1,8 @@
+#pragma once
+	
+#include<vector>
+
+int gcd(int a, int b);
+int lcm(int a, int b);
+int gcdv(const std::vector<int>& v);
+int lcmv(const std::vector<int>& v);
