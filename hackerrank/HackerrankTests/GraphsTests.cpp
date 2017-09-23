c6adc810975c9d483a5a54f53982794b81acb787
sendivogius <sendivogius@gmail.com>
2017-09-23 16:11:28 +0200

    Add more graph test for neightbours

--- hackerrank/HackerrankTests/GraphsTests.cpp
+++ hackerrank/HackerrankTests/GraphsTests.cpp
@@ -31,5 +31,35 @@ namespace HackerrankTests
 		{
 			Assert::IsTrue(testGraph.edgesCount() == 6);
 		}
+
+		TEST_METHOD(Graph1Neigbours)
+		{			
+			{
+			int tmp[] = {1, 4, 3};
+			const vector<int> neighbours( tmp, tmp+(sizeof(tmp)/sizeof(tmp[0])));
+			Assert::IsTrue(testGraph.getNeighbours(0) == neighbours);
+			}
+			{
+			int tmp[] = {0, 4};
+			const vector<int> neighbours( tmp, tmp+(sizeof(tmp)/sizeof(tmp[0])));
+			Assert::IsTrue(testGraph.getNeighbours(1) == neighbours);
+			}
+			{
+			int tmp[] = {3};
+			const vector<int> neighbours( tmp, tmp+(sizeof(tmp)/sizeof(tmp[0])));
+			Assert::IsTrue(testGraph.getNeighbours(2) == neighbours);
+			}
+			{
+			int tmp[] = {0, 2, 4};
+			const vector<int> neighbours( tmp, tmp+(sizeof(tmp)/sizeof(tmp[0])));
+			Assert::IsTrue(testGraph.getNeighbours(3) == neighbours);
+			}
+			{
+			int tmp[] = {0, 1, 3};
+			const vector<int> neighbours( tmp, tmp+(sizeof(tmp)/sizeof(tmp[0])));
+			Assert::IsTrue(testGraph.getNeighbours(4) == neighbours);
+			}
+
+		}
 	};
 }
