6f2360311ae8014d6cbd360e29af0dcc487bd9e6
sendivogius <sendivogius@gmail.com>
2017-12-30 20:07:12 +0100

    Add picking-numbers.cpp

--- hackerrank/hackerrank/save_the_prisoner.cpp
+++ hackerrank/hackerrank/save_the_prisoner.cpp
@@ -9,13 +9,13 @@ long long calc_prisoner(long long sweets, long long start, long long prisoners)
 	return (start+sweets-2)%prisoners + 1;
 }
 
-int main() {
-    int t;
-    long long n, m, s;
-    cin >> t;
-    while(t--){
-        cin >> n >> m >> s;
-        cout << calc_prisoner(m, s, n) << endl;
-    }
-    return 0;
-}
+//int main() {
+//    int t;
+//    long long n, m, s;
+//    cin >> t;
+//    while(t--){
+//        cin >> n >> m >> s;
+//        cout << calc_prisoner(m, s, n) << endl;
+//    }
+//    return 0;
+//}
