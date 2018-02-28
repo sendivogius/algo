fed261b6a435b0adc425a597a5d5aaa97e99d884
sendivogius <sendivogius@gmail.com>
2018-02-28 21:50:25 +0100

    exam rush

---
+++ Hackerrank/Hackerrank/server_room_safety.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool checkLeft(vector<int>& height, vector<int>& position)
+{
+	for(int i = 0; i < height.size()-1; i++)
+	{
+		if(position[i]+height[i] >= position[i+1])
+			return true;
+	}
+	return false;
+
+}
+
+string checkAll(int n, vector<int>& height, vector<int>& position) {
+	const bool left = checkLeft(height, position);	
+	std::reverse(std::begin(height), std::end(height));
+	std::reverse(std::begin(position), std::end(position));
+	const bool right = checkLeft(height, position);
+	if(left && right)
+		return "BOTH";
+	if(left)
+		return "LEFT";
+	if(right)
+		return "RIGHT";
+	return "NONE";
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> position(n);
+    for(int position_i = 0; position_i < n; position_i++){
+       cin >> position[position_i];
+    }
+    vector<int> height(n);
+    for(int height_i = 0; height_i < n; height_i++){
+       cin >> height[height_i];
+    }
+    string ret = checkAll(n, height, position);
+    cout << ret << endl;
+    return 0;
+}
+
--- hackerrank/hackerrank/exam_rush.cpp
+++ hackerrank/hackerrank/exam_rush.cpp
@@ -17,19 +17,19 @@ long examRush(min_queue& minq, int t) {
 	return c;
 
 }
-
-int main() {
-    int n;
-    int t;
-    cin >> n >> t;
-    min_queue min_heap;
-    while(n--){
-        long l;
-        cin >> l;
-        min_heap.push(l);
-    }
-    long result = examRush(min_heap, t);
-    cout << result << endl;
-    return 0;
-}
+//
+//int main() {
+//    int n;
+//    int t;
+//    cin >> n >> t;
+//    min_queue min_heap;
+//    while(n--){
+//        long l;
+//        cin >> l;
+//        min_heap.push(l);
+//    }
+//    long result = examRush(min_heap, t);
+//    cout << result << endl;
+//    return 0;
+//}
 
