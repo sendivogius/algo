b92fce2d7e1d924fb0f0a8a28218ad5bf6767aec
sendivogius <sendivogius@gmail.com>
2018-03-06 22:16:27 +0100

    Delete lowercase hackerrank dir

--- hackerrank/hackerrank/between_two_sets.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include<vector>
-using namespace std;
-
-//https://www.hackerrank.com/challenges/between-two-sets
-
-int getTotalX(const vector<int>& a, const vector<int>& b);
--- hackerrank/hackerrank/math_utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-	
-#include<vector>
-
-int gcd(int a, int b);
-int lcm(int a, int b);
-int gcdv(const std::vector<int>& v);
-int lcmv(const std::vector<int>& v);
-
